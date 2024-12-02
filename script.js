c1f7ab4a5c029c9691a16ac71770911e42c6411d
Mohamed Haitham <mo7amed.yamani@gmail.com>
2024-12-02 22:03:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to toggle the visibility of the navigation menu
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links'); // Select the nav links container
+    navLinks.classList.toggle('open'); // Toggle the "open" class
+}
