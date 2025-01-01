8ccc5777c9bbd7c26e8f9c1b5dad388725dcbb93
Mohamed Haitham <mo7amed.yamani@gmail.com>
2025-01-01 03:23:06 +0200

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Function to toggle the visibility of the navigation menu
 function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links'); // Select the nav links container
-    navLinks.classList.toggle('open'); // Toggle the "open" class
+    const navLinks = document.querySelector('.nav-links'); 
+    navLinks.classList.toggle('open'); 
 }
